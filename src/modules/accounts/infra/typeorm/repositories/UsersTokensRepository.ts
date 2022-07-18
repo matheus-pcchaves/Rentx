66c7a75bb2530fb278de7f33d9e0457522c55669
Matheus <vicde2@hotmail.com>
2022-07-18 13:44:08 -0300

    Email e recuperação de senha

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -12,7 +12,7 @@ class UsersTokensRepository implements IUsersTokensRepository{
     constructor(){
         this.repository = getRepository(UserTokens)
     }
-    
+        
     async create({ expires_date, refresh_token, user_id }: ICreateUserTokenDTO): Promise<UserTokens> {
         const userTokens = this.repository.create({
             expires_date,
@@ -33,6 +33,12 @@ class UsersTokensRepository implements IUsersTokensRepository{
 
     async deleteById(id: string): Promise<void> {
         await this.repository.delete(id)
+    }
+
+    async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+        const userToken = await this.repository.findOne({refresh_token})
+
+        return userToken
     }
 }
 
--- src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -6,7 +6,7 @@ import { IDateProvider } from "../IDateProvider";
 dayjs.extend(utc)
 
 class DayjsDateProvider implements IDateProvider {
-    
+            
     compareInHours(start_date: Date, end_date: Date): number{
         const end_date_utc = this.convertToUTC(end_date)
         const start_date_utc = this.convertToUTC(start_date)
@@ -31,6 +31,14 @@ class DayjsDateProvider implements IDateProvider {
 
     addDays(days: number): Date {
         return dayjs().add(days, "days").toDate()
+    }
+
+    addHours(hours: number): Date {
+        return dayjs().add(hours, "hour").toDate()
+    }
+
+    compareIfBefore(start_date: Date, end_date: Date): boolean {
+        return dayjs(start_date).isBefore(end_date)
     }
 }
 
