556e3736f22b1bfa12e6e7d23f58f25830bd0260
Matheus <vicde2@hotmail.com>
2022-07-25 14:25:06 -0300

    Upload/update de imagens de carro

--- src/modules/cars/useCases/UploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/UploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe"
 
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository"
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider"
 
 interface IRequest {
     car_id: string,
@@ -12,12 +13,15 @@ class UploadCarImagesUseCase {
 
     constructor(
         @inject("CarsImageRepository")
-        private carsImagesRepository: ICarsImagesRepository
+        private carsImagesRepository: ICarsImagesRepository,
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
     ){}
 
     async execute({car_id, image_name}: IRequest): Promise<void>{
         image_name.map(async (image) => {
             await this.carsImagesRepository.create(car_id, image)
+            await this.storageProvider.save(image, "cars")
         })
     }
 }
