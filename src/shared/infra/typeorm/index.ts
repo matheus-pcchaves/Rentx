756c34f546ea0db7a6b7c0c4ffdc11175614d865
Matheus <vicde2@hotmail.com>
2022-08-04 17:20:19 -0300

    rentx

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,14 +1,15 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async(): Promise<Connection> => {
+export default async(host = 'database_ignite'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions()
 
   return createConnection(
     Object.assign(defaultOptions, {
+      host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
           : defaultOptions.database 
     })
   )
-} 
+}
